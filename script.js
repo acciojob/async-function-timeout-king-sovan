1c19d6b0f4fb715821e8193da2114a4f59164855
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-22 20:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function displayMessage() {
+	const text = document.getElementById('text').value;
+	const delayTime = document.getElementById('delay').value;
+
+	await delay(delayTime);
+
+	document.getElementById('output').textContent = text;
+}
+
+document.getElementById('btn').addEventListener('click', displayMessage);
